d8de8562e152f5f1d844c13c16b96705b2f63fb8
danieldafoe <danieldafoe@l-178-160.ddi.sheridanc.on.ca>
2014-03-19 13:57:50 -0400

    Various changes.
    - Too many to list.

--- www/js/map.js
+++ www/js/map.js
@@ -1,3 +1,4 @@
+/*
 var map;
 function initialize() {
   var mapOptions = {
@@ -9,3 +10,25 @@ function initialize() {
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
+
+
+function initialize() {
+	var myOptions = {
+        center: new google.maps.LatLng(43.591525,-79.638069),
+        zoom: 8,   
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+};
+var map = new google.maps.Map(document.getElementById("map-canvas"),myOptions);
+*/
+function initialize() {
+    // Also works with: var yourStartLatLng = '59.3426606750, 18.0736160278';
+    var LatLng = new google.maps.LatLng(59.3426606750, 18.0736160278);
+    $('#map-canvas').gmap({'center': LatLng});
+    
+    $('#home').live("pageshow", function() {
+                $('#map-canvas').gmap('refresh');
+        });
+    $('#home').live("pageinit", function() {
+                $('#map-canvas').gmap({'center': '59.3426606750, 18.0736160278'});
+    });
+});
