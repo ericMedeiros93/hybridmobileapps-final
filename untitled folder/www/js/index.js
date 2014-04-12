d6eca51d3ff00ac0e2120c94371a07dd6e943145
ericMedeiros93 <littlechico93@gmail.com>
2014-04-11 20:20:04 -0400

    First commit
    First! Seem to fix sign in button bug

--- untitled folder/www/js/index.js
+++
@@ -1,4 +0,0 @@
-$(document).on('pagecreate', '#route', function() {
-	var user=localStorage.getItem("username");
-	$('#curUser').text("Signed in as: " + user);
-});
--- untitled folder/www/js/objects.js
+++
@@ -1,8 +0,0 @@
-<script type="text/javascript">
-function account(username,password,type)
-{
-this.username=username;
-this.password=password;
-this.type=type;
-}
-</script>
